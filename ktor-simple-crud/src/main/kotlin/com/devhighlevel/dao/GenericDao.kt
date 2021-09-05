22a3e4dea991e89b1863e0567a204df75e7f816e
Luis <lacarbonel2017@gmail.com>
2021-09-05 01:38:16 -0300

    Complete dynamo crud example

--- ktor-simple-crud/src/main/kotlin/com/devhighlevel/dao/GenericDao.kt
+++ ktor-simple-crud/src/main/kotlin/com/devhighlevel/dao/GenericDao.kt
@@ -2,7 +2,7 @@ package com.devhighlevel.dao
 
 interface GenericDao<E> {
     suspend fun save(entity: E): E?
-    fun findAll(): List<E>?
-    fun update(entity: E): E?
-    fun delete(id: String): E?
+    suspend fun findAll(): List<E>?
+    suspend fun update(entity: E): E?
+    suspend fun delete(id: String): E?
 }
--- ktor-simple-crud/src/main/kotlin/com/devhighlevel/routes/UserRoute.kt
+++ ktor-simple-crud/src/main/kotlin/com/devhighlevel/routes/UserRoute.kt
@@ -3,6 +3,7 @@ package com.devhighlevel.routes
 import com.devhighlevel.domain.dto.request.UserDto
 import com.devhighlevel.service.UserService
 import io.ktor.application.*
+import io.ktor.features.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
@@ -15,5 +16,20 @@ fun Route.userRoutes() {
     post {
         val userDto = call.receive<UserDto>()
         call.respond(userService.save(userDto))
+    }
+
+    put("/{id}") {
+        val id = call.parameters["id"] ?: throw NotFoundException("Not exist parameter [ id ]")
+        val userDto = call.receive<UserDto>()
+        call.respond(userService.update(userDto, id))
+    }
+
+    delete("/{id}") {
+        val id = call.parameters["id"] ?: throw NotFoundException("Not exist parameter [ id ]")
+        call.respond(userService.delete(id))
+    }
+
+    get {
+        call.respond(userService.findAll())
     }
 }
